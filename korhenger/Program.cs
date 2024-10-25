c3ab996072d424a3e2a3080343afea3077ffbb37
LeDDaniel <lentedaniel2@gmail.com>
2024-10-25 13:02:09 +0200

    2024

---
+++ korhenger/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace korhenger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            kor k1 = new kor();
+            k1.SetSugar(15);
+            k1.SetTerulet();
+            k1.SetKerulet();
+            Console.WriteLine($"A {k1.GetSugar()} sugarú kör kerülete:{k1.GetKerulet()},területe: {k1.GetTerulet()}.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ korhenger/kor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace korhenger
+{
+    class kor
+    {
+        private double sugar;
+        private double terulet;
+        private double kerulet;
+
+        public kor() { }
+        public kor(double r) {
+            sugar = r;
+                }
+        public void SetSugar (double r)
+        {
+            sugar = r;
+        }
+        public void SetTerulet()
+            {
+            this.terulet = Math.Pow(this.sugar, 2) * Math.PI;
+
+        }
+        public void SetKerulet()
+        {
+            this.kerulet = 2* this.sugar     * Math.PI;
+
+        }
+        public double GetTerulet()
+        {
+            return this.terulet;
+        }
+        public double GetKerulet()
+        {
+            return this.kerulet;
+            
+        }
+        public double GetSugar()
+        {
+            return this.sugar;
+        }
+    }
+}
