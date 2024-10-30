5e2d788676d34a7d9f3ac4b4a11361b09c8b8322
LeDDaniel <lentedaniel2@gmail.com>
2024-10-30 12:31:06 +0100

    2024.10.30 1

---
+++ KalkulátorOOP/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalkulátorOOP
+{
+    public class Calc
+    {
+      private double szam1;
+        private double szam2;
+        private char muvelet;
+        private double eredmeny;
+
+        public Calc() {}
+
+        public Calc(double szam1, double szam2, char muvelet)
+        {
+            this.szam1 = szam1;
+            this.szam2 = szam2;
+            this.muvelet = muvelet;
+        }
+
+        public void SetSzám1(double szam) => szam1 = szam;
+        public void SetSzám2(double szam) => szam2 = szam;
+        public void SetMűveletJel(char jel) => muvelet = jel;
+
+        public void SetEredmény()
+        {
+            switch (muvelet)
+            {
+                case '+': eredmeny = szam1 + szam2; break;
+                case '-': eredmeny = szam1 - szam2; break;
+                case '*': eredmeny = szam1 * szam2; break;
+                case '/': eredmeny = szam2 != 0 ? szam1 / szam2 : 0; break;
+                default: throw new InvalidOperationException("Ismeretlen művelet");
+            }
+        }
+
+        public double GetEredmény() => eredmeny;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc calc = new Calc(10, 5, '+');
+            calc.SetEredmény();
+            Console.WriteLine("Az eredmény: " + calc.GetEredmény());
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ KőPapírOllóOOP/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KőPapírOllóOOP
+{
+    using System;
+
+    namespace KőPapírOllóOOP
+    {
+        public class Kpo
+        {
+            private string tip1;
+            private string tip2;
+            private string eredmeny;
+
+            public Kpo() { }
+
+            public Kpo(string tip1, string tip2)
+            {
+                this.tip1 = tip1;
+                this.tip2 = tip2;
+            }
+
+            public void SetTip1(string tip) => tip1 = tip;
+            public void SetTip2(string tip) => tip2 = tip;
+
+            public void SetEredmény()
+            {
+                if (tip1 == tip2) eredmeny = "Döntetlen";
+                else if ((tip1 == "Kő" && tip2 == "Olló") || (tip1 == "Papír" && tip2 == "Kő") || (tip1 == "Olló" && tip2 == "Papír"))
+                    eredmeny = "Első játékos nyert";
+                else
+                    eredmeny = "Második játékos nyert";
+            }
+
+            public string GetEredmény() => eredmeny;
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Kpo kpo = new Kpo("Kő", "Olló");
+                kpo.SetEredmény();
+                Console.WriteLine("A játék eredménye: " + kpo.GetEredmény());
+            }
+        }
+    }
+}
