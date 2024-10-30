8ee7a1c0b458bd9a06bde4c493326e0c859be7a3
LeDDaniel <lentedaniel2@gmail.com>
2024-10-30 12:53:36 +0100

    5 óra

---
+++ SzámTesztOOP/Program.cs
@@ -0,0 +1,46 @@
+﻿íusing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzámTesztOOP
+{
+    public class SzámTeszt
+    {
+        private int szam1;
+        private int szam2;
+        private int eredmeny;
+
+        public SzámTeszt() { }
+
+        public SzámTeszt(int szam1, int szam2)
+        {
+            this.szam1 = szam1;
+            this.szam2 = szam2;
+        }
+
+        public void SetSzám1(int szam) => szam1 = szam;
+        public void SetSzám2(int szam) => szam2 = szam;
+
+        public void SetEredmény()
+        {
+            if (szam1 == szam2) eredmeny = 0;
+            else if (szam1 > szam2) eredmeny = 1;
+            else eredmeny = 2;
+        }
+
+        public int GetEredmény() => eredmeny;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SzámTeszt szamTeszt = new SzámTeszt(7, 5);
+            szamTeszt.SetEredmény();
+            Console.WriteLine("Az eredmény: " + szamTeszt.GetEredmény());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ VektorMinMaxOOP/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VektorMinMaxOOP
+{
+    public class VektorMinMax
+    {
+        private int[] vektor;
+        private int min;
+        private int minIndex;
+        private int max;
+        private int maxIndex;
+
+        public VektorMinMax() { }
+
+        public VektorMinMax(int[] vektor)
+        {
+            SetVektor(vektor);
+        }
+
+        public void SetVektor(int[] vektor)
+        {
+            this.vektor = vektor;
+            SetMin();
+            SetMax();
+        }
+
+        public void SetMin()
+        {
+            min = vektor.Min();
+            minIndex = Array.IndexOf(vektor, min);
+        }
+
+        public void SetMax()
+        {
+            max = vektor.Max();
+            maxIndex = Array.IndexOf(vektor, max);
+        }
+
+        public (int, int) GetMin() => (min, minIndex);
+        public (int, int) GetMax() => (max, maxIndex);
+
+        public (int, int)[] GetMinMax() => new[] { (min, minIndex), (max, maxIndex) };
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VektorMinMax vektorMinMax = new VektorMinMax(new[] { 3, 8, 1, 9, 2 });
+            Console.WriteLine("A legkisebb elem és indexe: " + vektorMinMax.GetMin());
+            Console.WriteLine("A legnagyobb elem és indexe: " + vektorMinMax.GetMax());
+            Console.ReadKey();
+        }
+    }
+}
