e93fd5e9c5dfd94c837ffbe627f3b9d3190d214f
LeDDaniel <lentedaniel2@gmail.com>
2024-11-25 11:59:57 +0100

    a

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("39a051c6-c10c-4560-92a7-2d30fd31694b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- korhengert/Kör_Teszt.cs
+++ korhengert/Kör_Teszt.cs
@@ -1,24 +1,71 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 using System;
-using korhenger;
-namespace korhengert
+
+using KörHenger;
+
+namespace Körhenger_UnitTest
+
 {
+
     [TestClass]
+
     public class Kör_Teszt
+
     {
+
         [TestMethod]
-        public void Kerület_Teszt()
+
+        public void Kerulet_Teszt()
+
+        {
+
+            // Arrange - Feltételek beállítása
+
+            double sugár = 5;
+
+            Kör Kör = new Kör(sugár);
+
+            double vártEredm =Math.PI*10;
+
+
+            // Act - Végrehajtás
+            
+            Kör.SetKerület();
+
+            double kapottEredm = Kör.GetKerület();
+
+            // Assert - Kiértékelés
+
+            Assert.AreEqual(vártEredm, kapottEredm);
+           
+
+        }
+
+        [TestMethod]
+
+        public void Terulet_Teszt()
+
         {
             double sugár = 5;
-            kor kor = new kor(sugár);
-            kor.SetKerulet();
-            double vártEredm=31.4;
-        }
-        [TestMethod]
-        public void Terület_Teszt()
-        {
-           
+
+            Kör Kör = new Kör(sugár);
+
+            double vártEredmt =31,42;
+
+
+            // Act - Végrehajtás
+            
+            Kör.SetTerület();
+
+            double kapottEredmt = Kör.GetTerület();
+
+            // Assert - Kiértékelés
+
+            Assert.AreEqual(vártEredmt, kapottEredmt);
         }
 
     }
+
 }
+
